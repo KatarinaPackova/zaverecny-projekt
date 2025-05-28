152158f2f4197c7c6a93386219b880cc66405f05
Katarina Packova <katarina.packova@gmail.com>
2025-05-28 18:15:30 +0200

    Uprava html a css - O nas

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,20 +3,36 @@ import './style.css';
 export const HomePage = () => {
   return (
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
+      <div>
+        <header>
+          <div className="header">
+            <div className="names">
+              <h1>Han a Katka</h1>
+            </div>
+            <div className="logo" />
+          </div>
+        </header>
+        <main>
+          <div className="article">
+            <h2 className="subheading"> O nás </h2>
+            <p className="katka--article">
+              Katka absolvovala kurz šití a občas si i něco sama pro sebe a
+              svoje blízké ušije, nebo upraví. Pracuje jako program manager v
+              oblasti vývoje aplikací a ráda by tyto dvě věci spojila dohromady
+              na realizaci tohoto užitečného projektu.
+            </p>
+            <p className="han--article">
+              Han studovala oděvní design a má roky zkušeností s klienty, kterým
+              se snažila pomoci s výběrem něčeho vhodného, nebo pro ně tvořila
+              na míru kousky z jejích kolekcí. Zajímá se také o udržitelnost a
+              není fanda masové produkce. Tyto zkušenosti chce propojit a využit
+              na tento projekt.
+            </p>
+          </div>
+        </main>
+      </div>
       <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
+        <p>© 2025 Katarina Packova, Han Frišonsová</p>
       </footer>
     </div>
   );
